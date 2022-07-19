6b0d02822a575de525226b4b92464b9fb5e89fc8
William Laurén <william.lauren@indmeas.com>
2022-07-19 18:18:51 +0300

    fix

--- encomp/units.pyi
+++ encomp/units.pyi
@@ -4,7 +4,6 @@ from typing import (Generic,
                     Literal,
                     Generator,
                     Any,
-                    Union,
                     overload,
                     SupportsAbs,
                     SupportsRound)
