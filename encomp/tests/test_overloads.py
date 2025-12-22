55388b903d35166dc9523bd32552fbc1aaecde8a
William Laurén <lauren.william.a@gmail.com>
2025-12-22 15:40:52 +0200

    test type checking

--- encomp/tests/test_overloads.py
+++ encomp/tests/test_overloads.py
@@ -1,7 +1,24 @@
+from typing import reveal_type
+
+import polars as pl
+
 from encomp.units import Quantity as Q
+from encomp.utypes import MassUnits
 
 
 def test_overloads() -> None:
-    v1 = Q(1, "kg")
+    # Test with explicit float
+    v1 = Q(1.0, "m")
+    reveal_type(v1)
 
-    v2 = v1 * 2  # noqa: F841
+    # Test with int
+    v2 = Q([1, 2, 3], "kg")
+    reveal_type(v2)
+
+    Q(pl.col.asd, "kg")
+    Q(pl.DataFrame({"test": []})["test"], "kg")
+
+    # Test with typed unit
+    unit: MassUnits = "kg"
+    v3 = Q(1.0, unit)
+    reveal_type(v3)
