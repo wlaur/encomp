807917736ebcc0541343bc1ab9f60f62faefef13
William Laurén <lauren.william.a@gmail.com>
2026-01-04 13:12:34 +0200

    add boilerplate

--- encomp/tests/test_transforms.py
+++ encomp/tests/test_transforms.py
@@ -1,6 +1,18 @@
-from typing import Any
+from typing import Any, assert_type
+
+from typeguard import typechecked
 
 from ..units import Quantity as Q
+
+
+def _assert_type(val: object, typ: type) -> None:
+    from encomp.misc import isinstance_types
+
+    if not isinstance_types(val, typ):
+        raise TypeError(f"Type mismatch for {val}: {type(val)}, expected {typ}")
+
+
+assert_type.__code__ = _assert_type.__code__
 
 
 def _q1() -> Q[Any, Any]:
@@ -12,5 +24,6 @@ def _q2(inp: Q[Any, Any]) -> Q[Any, Any]:
 
 
 # TODO: test with typeguard.typechecked
+# install typechecked on all funcs (no need to test the @decorator version)
 def test_transforms() -> None:
-    _ = _q2(_q1())
+    _ = typechecked(_q2)(typechecked(_q1)())
